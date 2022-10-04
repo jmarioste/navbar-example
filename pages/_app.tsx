44c61d0d4c80a4b07195bef75f0055e281b75f85
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-10-04 21:57:58 +0800

    Implement navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import DrawerLayout from "../components/DrawerLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DrawerLayout>
+      <Component {...pageProps} />
+    </DrawerLayout>
+  );
 }
 
-export default MyApp
+export default MyApp;
